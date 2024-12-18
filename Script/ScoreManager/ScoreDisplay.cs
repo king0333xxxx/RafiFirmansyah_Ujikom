14a14f50e794dd78bd9509d0abb072e75b27ac69
RafiFirmansyah <124896678+king0333xxxx@users.noreply.github.com>
2024-12-18 12:32:26 +0700

    Update 1.3 : Add Time System
    Add Time

--- Script/ScoreManager/ScoreDisplay.cs
+++ Script/ScoreManager/ScoreDisplay.cs
@@ -5,12 +5,32 @@ using TMPro;
 public class ScoreDisplay : MonoBehaviour
 {
     [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text timeText;
+
+    private float elapsedTime = 0f;
 
     void Update()
     {
+        // Update waktu
+        elapsedTime += Time.deltaTime;
+
+        // Format waktu (menit:detik)
+        string formattedTime = FormatTime(elapsedTime);
+
+        //update score
         if (ScoreManager.Instance != null)
         {
             scoreText.text = "Score: " + ScoreManager.Instance.GetCurrentScore();
         }
+
+        timeText.text = "Time: " + formattedTime;
+
+    }
+
+    private string FormatTime(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60); // Konversi ke menit
+        int seconds = Mathf.FloorToInt(time % 60); // Sisa detik
+        return $"{minutes:00}:{seconds:00}"; // Format ke "MM:SS"
     }
 }
